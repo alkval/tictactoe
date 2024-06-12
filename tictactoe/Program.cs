f5993d0ce8b0ccc6584e795f55a14aa701c728d1
Alexander <alexanderkvalvaag@gmail.com>
2024-06-12 15:58:27 +0200

    cleaned up the code and removed some trivial stuff

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -1,27 +1,49 @@
-﻿using System.Data.Common;
-
+﻿
 Console.WriteLine("Welcome to Tic Tac Toe!");
 
 Console.WriteLine("Player1 ( X ) vs Player2 ( O )");
 
 ConsoleKeyInfo key;
 int option = 0; 
-(int left, int top) = Console.GetCursorPosition();
 int turn = 0;
 int winner = 0;
 string marker = "_";
 string markerx = "X";
 string markero = "O";
 string markerb = " ";
+Console.CursorVisible = false;
 
 int [] options = [0, 0, 0, 
                   0, 0, 0, 
                   0, 0, 0];
 
+
+void printRow(int rowNumber)
+{
+    if (rowNumber == 1)
+    {
+        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
+        Console.WriteLine($"-{(option == 0 ? marker : "-")}-|-{(option == 1 ? marker : "-")}-|-{(option == 2 ? marker : "-")}-");
+
+    }
+    if (rowNumber == 2)
+    {
+        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
+        Console.WriteLine($"-{(option == 3 ? marker : "-")}-|-{(option == 4 ? marker : "-")}-|-{(option == 5 ? marker : "-")}-");
+
+    }
+    if (rowNumber == 3)
+    {
+        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        Console.WriteLine($" {(option == 6 ? marker : " ")}   {(option == 7 ? marker : " ")}   {(option == 8 ? marker : " ")} ");
+
+    }
+}
+
+
 while (winner == 0)
 {
     Console.Clear();
-    Console.SetCursorPosition(left, top);
 
     if (turn % 2 == 0)
     {
@@ -31,13 +53,11 @@ while (winner == 0)
     {
         Console.WriteLine("\nPlayer2's turn: O\n");
     }
-    //TODO : Make the WriteLines it into a function to make the code cleaner and more readable
-    Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-    Console.WriteLine($"-{(option == 0 ? marker : "-")}-|-{(option == 1 ? marker : "-")}-|-{(option == 2 ? marker : "-")}-");
-    Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-    Console.WriteLine($"-{(option == 3 ? marker : "-")}-|-{(option == 4 ? marker : "-")}-|-{(option == 5 ? marker : "-")}-");
-    Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
-    Console.WriteLine($" {(option == 6 ? marker : " ")}   {(option == 7 ? marker : " ")}   {(option == 8 ? marker : " ")} ");
+
+    printRow(1);
+    printRow(2);
+    printRow(3);
+
     key = Console.ReadKey(true);
 
     switch (key.Key)
@@ -79,99 +99,80 @@ while (winner == 0)
     {
         Console.Clear();
         winner = options[0];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
-        
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[3] == options[4] && options[4] == options[5] && options[3] != 0)
     {
         Console.Clear();
         winner = options[3];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[6] == options[7] && options[7] == options[8] && options[6] != 0)
     {
         Console.Clear();
         winner = options[6];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[0] == options[3] && options[3] == options[6] && options[0] != 0)
     {
         Console.Clear();
         winner = options[0];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[1] == options[4] && options[4] == options[7] && options[1] != 0)
     {
         Console.Clear();
         winner = options[1];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[2] == options[5] && options[5] == options[8] && options[2] != 0)
     {
         Console.Clear();
         winner = options[2];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[0] == options[4] && options[4] == options[8] && options[0] != 0)
     {
         Console.Clear();
         winner = options[0];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[2] == options[4] && options[4] == options[6] && options[2] != 0)
     {
         Console.Clear();
         winner = options[2];
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
     if (options[0] != 0 && options[1] != 0 && options[2] != 0 && options[3] != 0 && options[4] != 0 && options[5] != 0 && options[6] != 0 && options[7] != 0 && options[8] != 0)
     {
         Console.Clear();
-        Console.WriteLine($" {(options[0] == 1 ? markerx + " |": options[0] == 2 ? markero +" |": options[0] == 0 ? markerb +" |" : "")} {(options[1] == 1 ? markerx + " |": options[1] == 2 ? markero +" |": options[1] == 0 ? markerb +" |" : "")} {(options[2] == 1 ? markerx : options[2] == 2 ? markero : options[2] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[3] == 1 ? markerx + " |": options[3] == 2 ? markero +" |": options[3] == 0 ? markerb +" |" : "")} {(options[4] == 1 ? markerx + " |": options[4] == 2 ? markero +" |": options[4] == 0 ? markerb +" |" : "")} {(options[5] == 1 ? markerx : options[5] == 2 ? markero : options[5] == 0 ? markerb : "")}"); 
-        Console.WriteLine($"---|---|---");
-        Console.WriteLine($" {(options[6] == 1 ? markerx + " |": options[6] == 2 ? markero +" |": options[6] == 0 ? markerb +" |" : "")} {(options[7] == 1 ? markerx + " |": options[7] == 2 ? markero +" |": options[7] == 0 ? markerb +" |" : "")} {(options[8] == 1 ? markerx : options[8] == 2 ? markero : options[8] == 0 ? markerb : "")}"); 
+        printRow(1);
+        printRow(2);
+        printRow(3);
         break;
     }
 
--- tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8295b53fbf67f81fb776ab6abe5a1a1e0ce2965b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd575eef84667baea863967901e79c476daa341b")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
