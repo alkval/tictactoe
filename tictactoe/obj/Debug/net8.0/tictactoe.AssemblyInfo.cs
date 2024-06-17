1c1e59f4f6f9e50144a310dc168914f233b62fd7
Alexander <alexanderkvalvaag@gmail.com>
2024-06-17 02:07:40 +0200

    update

--- tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd575eef84667baea863967901e79c476daa341b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5993d0ce8b0ccc6584e795f55a14aa701c728d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
